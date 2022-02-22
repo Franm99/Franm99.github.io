7ef16f3c934ba7f706bd93931556a10b0b19eeff
Fran Moreno <fran_99_cbm@hotmail.com>
2022-02-22 18:21:28 +0100

    Initializing common layout

---
+++ js/layout.js
@@ -0,0 +1,27 @@
+/* Common header */
+let appHeader = `
+<nav>
+<ul>
+  <li>
+    <a href="index.html">Home</a>
+  </li>
+  <li>
+    <a href="projects.html">Projects</a>
+  </li>
+  <li>
+    <a href="about.html">About</a>
+  </li>
+  <li>
+    <a href="contact.html">Contact</a>
+  </li>
+</ul>
+</nav>
+`;
+document.getElementById("app-header").innerHTML = appHeader;
+
+/* Common footer */
+let appFooter = `
+Footer info
+`;
+document.getElementById("app-footer").innerHTML = appFooter;
+
